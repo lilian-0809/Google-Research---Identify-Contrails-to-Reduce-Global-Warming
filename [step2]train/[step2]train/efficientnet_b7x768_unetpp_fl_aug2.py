aaf7316bf4a88ad68393519c9e14d1b78dbc6d96
yi sun <yisun080963@gmail.com>
2026-01-04 20:43:38 +0900

    initial commit

---
+++ [step2]train/[step2]train/efficientnet_b7x768_unetpp_fl_aug2.py
@@ -0,0 +1,340 @@
+#!pip install transformers
+#!pip install segmentation-models-pytorch==0.3.3
+#!pip install safetensors==0.3.1
+
+from pathlib import Path
+import os
+import random
+import math
+from collections import defaultdict
+import cv2
+import skimage
+
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+
+import torch
+from torch import nn
+from torchvision import transforms
+import torchvision.transforms as T
+from torch.utils.data import Dataset, DataLoader
+
+from PIL import Image
+from tqdm.notebook import tqdm
+from transformers import get_cosine_schedule_with_warmup
+
+import segmentation_models_pytorch as smp
+
+import safetensors
+
+class Config:
+    train = True
+    
+    num_epochs = 24
+    num_classes = 1
+    batch_size = 8
+    seed = 42
+    
+    encoder = 'efficientnet-b7'
+    pretrained = True
+    weights = 'imagenet'
+    classes = ['contrail']
+    activation = None
+    in_chans = 3
+    
+    device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
+    
+    image_size = 768
+    warmup = 0
+    lr = 1e-4
+    
+class Paths:
+    #data_root = '/kaggle/input/google-research-identify-contrails-reduce-global-warming'
+    contrails = '../data/contrails/'
+    train_path = '../data/train_df.csv'
+    valid_path = '../data/valid_df.csv'
+    save_dir = './save'
+    #pretrain = 'https://huggingface.co/timm/tf_efficientnetv2_xl.in21k_ft_in1k/resolve/main/model.safetensors'
+    
+def set_seed(seed=1234):
+    random.seed(seed)
+    np.random.seed(seed)
+    os.environ["PYTHONHASHSEED"] = str(seed)
+    
+    torch.manual_seed(seed)
+    torch.cuda.manual_seed(seed)
+    torch.backends.cudnn.deterministic = False
+    torch.backends.cudnn.benchmark = True
+    
+os.makedirs(Paths.save_dir, exist_ok=True)
+set_seed(9)
+
+# Import dataframes
+train_df = pd.read_csv(Paths.train_path)
+valid_df = pd.read_csv(Paths.valid_path)
+
+train_df['path'] = Paths.contrails + train_df['record_id'].astype(str) + '.npy'
+valid_df['path'] = Paths.contrails + valid_df['record_id'].astype(str) + '.npy'
+
+train_df.shape, valid_df.shape
+
+class ContrailsDataset(torch.utils.data.Dataset):
+    def __init__(self, df, image_size=256, train=True):
+
+        self.df = df
+        self.trn = train
+        self.normalize_image = T.Normalize((0.485, 0.456, 0.406), (0.229, 0.224, 0.225))
+        self.image_size = image_size
+        if image_size != 256:
+            self.resize_image = T.transforms.Resize(image_size)
+
+    def __getitem__(self, index):
+        row = self.df.iloc[index]
+        con_path = row.path
+        con = np.load(str(con_path))
+
+        img = con[..., :-1]
+        label = con[..., -1]
+        
+        if self.trn:
+            p = np.random.randint(6)
+            if p==1:
+                label = np.rot90(label, k=1).copy()
+                img = np.rot90(img, k=1).copy()
+                
+            #if p==2:
+            #    label = np.rot90(label, k=2).copy()
+            #    img = np.rot90(img, k=2).copy()
+                
+            #if p==3:
+            #    label = np.rot90(label, k=3).copy()
+            #    img = np.rot90(img, k=3).copy()
+                
+                
+            if p==2:
+                label = label[::-1].copy()
+                img = img[::-1].copy()
+                
+            #if p==5:
+            #    label = label[:, ::-1].copy()
+            #    img = img[:, ::-1].copy()
+                
+            if p==3:
+                label = np.rot90(label[:, ::-1]).copy()
+                img = np.rot90(img[:, ::-1]).copy()
+                
+            #if p==7:
+            #    label = np.rot90(label[:, ::-1], k=3).copy()
+            #    img = np.rot90(img[:, ::-1], k=3).copy()
+        
+        label = torch.tensor(label)
+
+        img = torch.tensor(np.reshape(img, (256, 256, 3))).to(torch.float32).permute(2, 0, 1)
+
+        if self.image_size != 256:
+            img = self.resize_image(img)
+
+        img = self.normalize_image(img)
+
+        return img.float(), label.float()
+
+    def __len__(self):
+        return len(self.df)
+
+train_ds = ContrailsDataset(
+        train_df,
+        Config.image_size,
+        train=True
+    )
+
+valid_ds = ContrailsDataset(
+        valid_df,
+        Config.image_size,
+        train=False
+    )
+
+train_dl = DataLoader(train_ds, batch_size=Config.batch_size , shuffle=True, num_workers = 8, drop_last=True)    
+valid_dl = DataLoader(valid_ds, batch_size=Config.batch_size, num_workers = 8, drop_last=True)
+
+img, label = next(iter(train_dl))
+img.shape, label.shape
+
+img, label = next(iter(valid_dl))
+img.shape, label.shape
+
+def dice_coef(y_true, y_pred, thr=0.5, epsilon=0.001):
+    y_true = y_true.flatten()
+    y_pred = (y_pred>thr).astype(np.float32).flatten()
+    inter = (y_true*y_pred).sum()
+    den = y_true.sum() + y_pred.sum()
+    dice = ((2*inter+epsilon)/(den+epsilon))
+    return dice
+
+class UNet(nn.Module):
+    def __init__(self, cfg):
+        super(UNet, self).__init__()
+        
+        self.cfg = cfg
+        self.training = True
+        
+        self.model = smp.UnetPlusPlus(
+            encoder_name=cfg.encoder, 
+            encoder_weights=cfg.weights, 
+            decoder_use_batchnorm=True,
+            classes=len(cfg.classes), 
+            activation=cfg.activation,
+        )
+        #self.model.encoder.model.load_state_dict(safetensors.torch.load_file(cfg.pretrain, device="cpu"), strict=False)
+        
+        self.loss_fn = smp.losses.FocalLoss(mode='binary', alpha=0.77, gamma=2.1)
+    
+    def forward(self, imgs, targets):
+        
+        x = imgs
+        y = targets
+
+        logits = self.model(x)
+        logits = torch.nn.functional.interpolate(logits, size=256, mode='bilinear')
+        loss = self.loss_fn(logits, y)
+        
+        return {"loss": loss, "logits": logits.sigmoid(), "logits_raw": logits, "target": y}
+
+def train_step(model, dataloader, optimizer, device):
+    
+    model.train()
+    scaler = torch.cuda.amp.GradScaler()
+    train_losses = []
+    pbar = tqdm(enumerate(dataloader), total=len(dataloader), desc='Train ', miniters=50, mininterval=60)
+    
+    for step, (X, y) in pbar:
+        torch.set_grad_enabled(True)
+        with torch.cuda.amp.autocast():
+            X, y = X.to(device), y.to(device)
+            #
+        
+            output_dict = model(X, y)
+        loss = output_dict["loss"]
+        train_losses.append(loss.item())
+        scaler.scale(loss).backward()
+        scaler.step(optimizer)
+        #loss.backward()
+        #optimizer.step()
+        scaler.update()
+        optimizer.zero_grad()
+        
+        if scheduler is not None:
+            scheduler.step()
+    
+    train_loss = np.sum(train_losses)
+    
+    return train_loss
+
+def test_step(model, dataloader, device):
+    
+    model.eval()
+    torch.set_grad_enabled(False)
+    
+    val_data = defaultdict(list)
+    pbar = tqdm(enumerate(dataloader), total=len(dataloader), desc='Valid', miniters=50, mininterval=60)
+    for step, (X, y) in pbar: 
+        X, y = X.to(device), y.to(device)
+
+        output = model(X, y)
+        for key, val in output.items():
+            val_data[key] += [output[key]]
+
+    for key, val in output.items():
+        value = val_data[key]
+        if len(value[0].shape) == 0:
+            val_data[key] = torch.stack(value)
+        else:
+            val_data[key] = torch.cat(value, dim=0).cpu().detach().numpy()
+    
+    val_losses = val_data["loss"].cpu().numpy()
+    val_loss = np.sum(val_losses)
+    
+    val_dice = dice_coef(val_data['target'], val_data['logits'])
+    
+    return val_loss, val_dice
+
+from tqdm.auto import tqdm
+
+
+def train(model, train_dataloader, test_dataloader, optimizer, epochs, device):
+    results = {'train_loss': [],
+              'val_loss': [],
+              'val_dice': []}
+    best = 0
+    es = 3
+    k = 0
+    for epoch in range(epochs):
+        
+        set_seed(Config.seed + epoch)
+        print("EPOCH:", epoch)
+        
+        train_loss = train_step(model,
+                              train_dataloader,
+                              optimizer,
+                              device)
+        val_loss, val_dice = test_step(model,
+                            test_dataloader,
+                            device)
+        
+        train_loss = train_loss / len(train_ds)
+        val_loss = val_loss / len(valid_ds)
+        
+        print(f'Train Loss: {train_loss:.4f} | Val Loss: {val_loss:.4f} | Val Dice: {val_dice:.4f}')
+        print(f"Learning rate: {optimizer.param_groups[0]['lr']}")
+        
+        results['train_loss'].append(train_loss)
+        results['val_loss'].append(val_loss)
+        results['val_dice'].append(val_dice)
+
+        
+        if val_dice> best:
+            best = val_dice
+            PATH = f"{Paths.save_dir}/unetpp_fl_{Config.encoder}_{Config.image_size}_ep{epoch}_dice{val_dice:.4f}_aug2.pth"
+            torch.save(model.state_dict(), PATH)
+            k = 0
+        else:
+            k +=1
+        if k >=es:
+            break
+        
+    return results
+def get_optimizer(lr, params):
+    
+    model_optimizer = torch.optim.AdamW(
+            filter(lambda p: p.requires_grad, params), 
+            lr=lr,
+            weight_decay=1e-6)
+    
+    return model_optimizer
+
+def get_scheduler(cfg, optimizer, total_steps):
+    scheduler = get_cosine_schedule_with_warmup(
+        optimizer,
+        num_warmup_steps= cfg.warmup * (total_steps // cfg.batch_size),
+        num_training_steps= cfg.num_epochs * (total_steps // cfg.batch_size)
+    )
+    return scheduler
+
+NUM_EPOCHS = Config.num_epochs
+model = UNet(Config).to(Config.device)
+
+total_steps = len(train_ds)
+optimizer = get_optimizer(lr=Config.lr, params=model.parameters())
+scheduler = get_scheduler(Config, optimizer, total_steps)
+
+from timeit import default_timer as timer
+start_time = timer()
+
+model_results = train(model, train_dl, valid_dl, optimizer, NUM_EPOCHS, Config.device)
+
+end_time = timer()
+
+# run.finish()
+print(f'Total Training Time: {end_time-start_time:.3f} seconds')
+
+
---
+++ [step2]train/[step2]train/unet_timm-efficientnet-b7_768_wd1.py
@@ -0,0 +1,234 @@
+# Dataset
+import yaml
+with open("config_unet_timm-efficientnet-b7_768_wd1", "r") as file_obj:
+    config = yaml.safe_load(file_obj)
+import torch
+import numpy as np
+import torchvision.transforms as T
+
+class ContrailsDataset(torch.utils.data.Dataset):
+    def __init__(self, df, image_size=256, train=True):
+
+        self.df = df
+        self.trn = train
+        self.normalize_image = T.Normalize((0.485, 0.456, 0.406), (0.229, 0.224, 0.225))
+        self.image_size = image_size
+        if image_size != 256:
+            self.resize_image = T.transforms.Resize(image_size)
+
+    def __getitem__(self, index):
+        row = self.df.iloc[index]
+        con_path = row.path
+        con = np.load(str(con_path))
+
+        img = con[..., :-1]
+        label = con[..., -1]
+        if self.trn:
+            p = np.random.randint(6)
+            if p==1:
+                label = np.rot90(label, k=1).copy()
+                img = np.rot90(img, k=1).copy()
+
+            #if p==2:
+            #    label = np.rot90(label, k=2).copy()
+            #    img = np.rot90(img, k=2).copy()
+
+            #if p==3:
+            #    label = np.rot90(label, k=3).copy()
+            #    img = np.rot90(img, k=3).copy()
+
+
+            if p==2:
+                label = label[::-1].copy()
+                img = img[::-1].copy()
+
+            #if p==5:
+            #    label = label[:, ::-1].copy()
+            #    img = img[:, ::-1].copy()
+
+            if p==3:
+                label = np.rot90(label[:, ::-1]).copy()
+                img = np.rot90(img[:, ::-1]).copy()
+
+            #if p==7:
+            #    label = np.rot90(label[:, ::-1], k=3).copy()
+            #    img = np.rot90(img[:, ::-1], k=3).copy()
+        label = torch.tensor(label)
+
+        img = torch.tensor(np.reshape(img, (256, 256, 3))).to(torch.float32).permute(2, 0, 1)
+
+        if self.image_size != 256:
+            img = self.resize_image(img)
+
+        img = self.normalize_image(img)
+
+        return img.float(), label.float()
+
+    def __len__(self):
+        return len(self.df)
+
+# Lightning module
+
+import torch
+import pytorch_lightning as pl
+import segmentation_models_pytorch as smp
+from torch.optim.lr_scheduler import CosineAnnealingLR, ReduceLROnPlateau
+from torch.optim import AdamW
+import torch.nn as nn
+from torchmetrics.functional import dice
+
+seg_models = {
+    "Unet": smp.Unet,
+    "Unet++": smp.UnetPlusPlus,
+    "MAnet": smp.MAnet,
+    "Linknet": smp.Linknet,
+    "FPN": smp.FPN,
+    "PSPNet": smp.PSPNet,
+    "PAN": smp.PAN,
+    "DeepLabV3": smp.DeepLabV3,
+    "DeepLabV3+": smp.DeepLabV3Plus,
+}
+
+
+class LightningModule(pl.LightningModule):
+    def __init__(self, config):
+        super().__init__()
+        self.config = config
+        self.model = seg_models[config["seg_model"]](
+            encoder_name=config["encoder_name"],
+            encoder_weights='noisy-student',
+            in_channels=3,
+            classes=1,
+            activation=None,
+        )
+        self.loss_module = smp.losses.FocalLoss(mode='binary', alpha=0.77, gamma=2.1)
+        self.val_step_outputs = []
+        self.val_step_labels = []
+
+    def forward(self, batch):
+        imgs = batch
+        preds = self.model(imgs)
+        return preds
+
+    def configure_optimizers(self):
+        optimizer = AdamW(self.parameters(), **self.config["optimizer_params"])
+
+        if self.config["scheduler"]["name"] == "CosineAnnealingLR":
+            scheduler = CosineAnnealingLR(
+                optimizer,
+                **self.config["scheduler"]["params"]["CosineAnnealingLR"],
+            )
+            lr_scheduler_dict = {"scheduler": scheduler, "interval": "step"}
+            return {"optimizer": optimizer, "lr_scheduler": lr_scheduler_dict}
+        elif self.config["scheduler"]["name"] == "ReduceLROnPlateau":
+            scheduler = ReduceLROnPlateau(
+                optimizer,
+                **self.config["scheduler"]["params"]["ReduceLROnPlateau"],
+            )
+            lr_scheduler = {"scheduler": scheduler, "monitor": "val_loss"}
+            return {"optimizer": optimizer, "lr_scheduler": lr_scheduler}
+
+    def training_step(self, batch, batch_idx):
+        imgs, labels = batch
+        preds = self.model(imgs)
+        if self.config["image_size"] != 256:
+            preds = torch.nn.functional.interpolate(preds, size=256, mode='bilinear')
+        loss = self.loss_module(preds, labels)
+        self.log("train_loss", loss, on_step=True, on_epoch=True, prog_bar=False, batch_size=16)
+
+        for param_group in self.trainer.optimizers[0].param_groups:
+            lr = param_group["lr"]
+        self.log("lr", lr, on_step=True, on_epoch=False, prog_bar=False)
+
+        return loss
+
+    def validation_step(self, batch, batch_idx):
+        imgs, labels = batch
+        preds = self.model(imgs)
+        if self.config["image_size"] != 256:
+            preds = torch.nn.functional.interpolate(preds, size=256, mode='bilinear')
+        loss = self.loss_module(preds, labels)
+        self.log("val_loss", loss, on_step=False, on_epoch=True, prog_bar=False)
+        self.val_step_outputs.append(preds)
+        self.val_step_labels.append(labels)
+
+    def on_validation_epoch_end(self):
+        all_preds = torch.cat(self.val_step_outputs)
+        all_labels = torch.cat(self.val_step_labels)
+        all_preds = torch.sigmoid(all_preds)
+        self.val_step_outputs.clear()
+        self.val_step_labels.clear()
+        val_dice = dice(all_preds, all_labels.long())
+        self.log("val_dice", val_dice, on_step=False, on_epoch=True, prog_bar=False)
+        if self.trainer.global_rank == 0:
+            print(f"\nEpoch: {self.current_epoch}", flush=True)
+
+# Actual training
+
+import warnings
+
+warnings.filterwarnings("ignore")
+
+import os
+import torch
+
+import pandas as pd
+import pytorch_lightning as pl
+from pprint import pprint
+from pytorch_lightning.callbacks import ModelCheckpoint, EarlyStopping, TQDMProgressBar
+from torch.utils.data import DataLoader
+
+
+
+contrails = os.path.join(config["data_path"], "contrails/")
+train_path = os.path.join(config["data_path"], "train_df.csv")
+valid_path = os.path.join(config["data_path"], "valid_df.csv")
+
+train_df = pd.read_csv(train_path).head(20512)
+valid_df = pd.read_csv(valid_path)#.head(1000)
+
+
+train_df["path"] = contrails + train_df["record_id"].astype(str) + ".npy"
+valid_df["path"] = contrails + valid_df["record_id"].astype(str) + ".npy"
+
+dataset_train = ContrailsDataset(train_df, config["model"]["image_size"], train=True)
+dataset_validation = ContrailsDataset(valid_df, config["model"]["image_size"], train=False)
+
+data_loader_train = DataLoader(
+    dataset_train,
+    batch_size=config["train_bs"],
+    shuffle=True,
+    num_workers=config["workers"],
+)
+data_loader_validation = DataLoader(
+    dataset_validation,
+    batch_size=config["valid_bs"],
+    shuffle=False,
+    num_workers=config["workers"],
+)
+
+checkpoint_callback = ModelCheckpoint(
+    save_weights_only=True,
+    monitor="val_dice",
+    dirpath=config["output_dir"],
+    mode="max",
+    filename=f"{config['model']['seg_model']}_{config['model']['encoder_name']}_{config['model']['image_size']}",
+    save_top_k=1,
+    verbose=1,
+)
+
+progress_bar_callback = TQDMProgressBar(
+    refresh_rate=config["progress_bar_refresh_rate"]
+)
+
+early_stop_callback = EarlyStopping(**config["early_stop"])
+
+trainer = pl.Trainer(
+    callbacks=[checkpoint_callback, early_stop_callback, progress_bar_callback],
+    **config["trainer"],
+)
+
+config["model"]["scheduler"]["params"]["CosineAnnealingLR"]["T_max"] *= len(data_loader_train)/config["trainer"]["devices"]
+model = LightningModule(config["model"])
+
+trainer.fit(model, data_loader_train, data_loader_validation)
